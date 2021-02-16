323a91b81c287a9434b9b0e50f5618522505c96d
root <root@ubuntu.jxdznbpqrufu3mlqc4hzxlvykf.bx.internal.cloudapp.net>
2021-02-16 11:38:40 +0000

    clustering

--- mllib/src/main/scala/com/yavuzozguven/Regression.scala
+++ mllib/src/main/scala/com/yavuzozguven/Regression.scala
@@ -9,7 +9,7 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions.{col, monotonically_increasing_id}
 
-object Main {
+object Regression {
   def main(args: Array[String]): Unit = {
     val spark = new SparkSession.Builder().master("local[*]").appName("MLlib").getOrCreate()
 
